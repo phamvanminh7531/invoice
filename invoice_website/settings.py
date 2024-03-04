4e37e42cf53a32c1c255b1c2172519c2cd9af6c3
phamvanminh7531 <phamvanminh7531@gmail.com>
2024-03-04 23:29:58 +0700

    repair for pythonanywhere

--- invoice_website/settings.py
+++ invoice_website/settings.py
@@ -12,8 +12,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -133,5 +131,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
